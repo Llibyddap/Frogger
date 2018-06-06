77e4c131960808e00e998a79e74215ef8e3cd117
explosion33 <38818379+explosion33@users.noreply.github.com>
2018-06-05 22:56:41 -0700

    Add files via upload

---
+++ frog.py
@@ -0,0 +1,16 @@
+from scene import *
+
+
+class createFrog (Node):
+	def __init__(self,tilesW,tilesH,sceneSize):
+		self.size = (sceneSize[1] * 0.75/tilesH, sceneSize[1] * 0.75/tilesH)
+		self.anchor_point = (0, 0)
+		frog = SpriteNode('frog.PNG')
+		frog.anchor_point = (0,0)
+		frog.size = self.size
+		self.add_child(frog)
+		self.position = (488.73, self.size[1]/4)
+		self.tile = ((self.position[0]/(sceneSize[0]/tilesW)), int(self.position[1]/(sceneSize[1]/tilesH)))
+		print(self.tile)
+	
+					
